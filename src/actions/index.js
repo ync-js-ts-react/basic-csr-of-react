b290e33e87cd46c7e1eaac48013598d7d6d2d3f2
GQSM <ms314006@hotmail.com>
2020-02-17 21:44:35 +0800

    [CHANGE] 把 api 的 http 改成 https

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_REQUEST_QUERY = 'fetch_request_query';
 
 export const fetchRequestQuery = async (dispatch) => {
-  const res = await axios.get('http://httpbin.org/get');
+  const res = await axios.get('https://httpbin.org/get');
 
   dispatch({
     type: FETCH_REQUEST_QUERY,
